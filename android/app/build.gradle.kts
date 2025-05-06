109e6e4b21244c0f108853998c1132ddc12bb741
yasinpalash <yasinmiapolash@gmail.com>
2025-05-06 16:59:11 +0600

    Update dependencies and refactor main application structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.riverpod_standard"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion= "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
