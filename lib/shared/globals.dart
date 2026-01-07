805ac3d16e0bfe771081c43501b94a925ed4d681
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-07 16:49:52 +0600

    theme setup

---
+++ lib/shared/globals.dart
@@ -0,0 +1,6 @@
+
+
+
+
+const String USER_LOCAL_STORAGE_KEY = 'user';
+const String APP_THEME_STORAGE_KEY = 'AppTheme';
--- lib/shared/theme/app_theme.dart
+++ lib/shared/theme/app_theme.dart
@@ -1,8 +1,68 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_standard/shared/data/local/storage_service.dart';
+import 'package:riverpod_standard/shared/theme/text_styles.dart';
+import 'package:riverpod_standard/shared/theme/text_theme.dart';
+import '../globals.dart';
+import 'app_colors.dart';
 
-// class AppThemeModeNotifier extends StateNotifier<ThemeData>{
-//
-// }
+// final appThemeProvider= StateNotifierProvider<AppThemeModeNotifier,ThemeMode>(
+//     (ref){
+//       return AppThemeModeNotifier();
+//     }
+// )
+class AppThemeModeNotifier extends StateNotifier<ThemeMode> {
+  final StorageService storageService;
+  ThemeMode currentTheme = ThemeMode.light;
+  AppThemeModeNotifier(this.storageService) : super(ThemeMode.light) {
+    getCurrentTheme();
+  }
+  void getCurrentTheme() async {
+    final theme = await storageService.get(APP_THEME_STORAGE_KEY);
+    final value = ThemeMode.values.byName('${theme ?? 'light'}');
+    state = value;
+  }
+}
+class AppTheme {
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      fontFamily: AppTextStyles.fontFamily,
+      primaryColor: AppColors.primary,
+      colorScheme: const ColorScheme.dark(
+        primary: AppColors.primary,
+        secondary: AppColors.lightGrey,
+        error: AppColors.error,
+        background: AppColors.black,
+      ),
+      // backgroundColor: AppColors.black,
+      scaffoldBackgroundColor: AppColors.black,
+      textTheme: TextThemes.darkTextTheme,
+      primaryTextTheme: TextThemes.primaryTextTheme,
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: AppColors.black,
+        titleTextStyle: AppTextStyles.h2,
+      ),
+    );
+  }
+
+  /// Light theme data of the app
+  static ThemeData get lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.primary,
+      textTheme: TextThemes.textTheme,
+      primaryTextTheme: TextThemes.primaryTextTheme,
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.primary,
+        secondary: AppColors.lightGrey,
+        error: AppColors.error,
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+      ),
+    );
+  }
+}
