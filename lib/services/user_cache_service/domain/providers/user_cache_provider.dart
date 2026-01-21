2a214b0d60f011ea0c3a976ebb24078dcea513ab
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-21 16:58:24 +0600

    user cache

--- lib/services/user_cache_service/domain/providers/user_cache_provider.dart
+++ lib/services/user_cache_service/domain/providers/user_cache_provider.dart
@@ -1,4 +1,10 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_standard/services/user_cache_service/domain/repositories/user_cache_repository.dart';
 
-// final userLocalRepositoryProvider=Provider<>((ref){});
+import '../../../../shared/domain/providers/shared_preferences_storage_service_provider.dart';
+
+// final userLocalRepositoryProvider=Provider<UserRepository>((ref){
+//   final storageService = ref.watch(storageServiceProvider);
+//
+// });
--- lib/services/user_cache_service/domain/repositories/user_cache_repository.dart
+++ lib/services/user_cache_service/domain/repositories/user_cache_repository.dart
@@ -1,3 +1,8 @@
+import '../../../../shared/domain/models/user/user_model.dart';
+
 abstract class UserRepository{
- // Future<bool> saveUser({ required User user});
+  // Future<Either<AppException, User>> fetchUser();
+ Future<bool> saveUser({ required User user});
+ Future<bool> deleteUser();
+ Future<bool> hasUser();
 }
