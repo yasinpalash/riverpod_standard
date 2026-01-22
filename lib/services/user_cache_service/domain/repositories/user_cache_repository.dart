281ce4071278ab0127dc97ac3d8990bb318d278b
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-22 15:55:41 +0600

    http set up

--- lib/services/user_cache_service/domain/repositories/user_cache_repository.dart
+++ lib/services/user_cache_service/domain/repositories/user_cache_repository.dart
@@ -1,7 +1,7 @@
 import '../../../../shared/domain/models/user/user_model.dart';
 
 abstract class UserRepository{
-  // Future<Either<AppException, User>> fetchUser();
+ // Future<Either<AppException, User>> fetchUser();
  Future<bool> saveUser({ required User user});
  Future<bool> deleteUser();
  Future<bool> hasUser();
