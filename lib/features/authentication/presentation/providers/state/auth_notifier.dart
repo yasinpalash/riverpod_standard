a21b047e8a005266e8edf6b3988fcc2e5b175f5c
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-27 16:59:40 +0600

    auth_state

--- lib/features/authentication/presentation/providers/state/auth_notifier.dart
+++ lib/features/authentication/presentation/providers/state/auth_notifier.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_standard/features/authentication/presentation/providers/state/auth_state.dart';
+import 'package:riverpod_standard/shared/domain/models/user/user_model.dart';
 import '../../../../../services/user_cache_service/domain/repositories/user_cache_repository.dart';
 import '../../../domain/repositories/auth_repository.dart';
 
@@ -8,4 +9,12 @@ class AuthNotifier extends StateNotifier<AuthState> {
   final UserRepository userRepository;
   AuthNotifier({required this.authRepository, required this.userRepository})
     : super(const AuthState.initial());
+
+  Future<void> loginUser(String username, String password) async {
+    state = const AuthState.loading();
+    final response = await authRepository.loginUser(
+      user: User(username: username, password: password),
+    );
+
+  }
 }
