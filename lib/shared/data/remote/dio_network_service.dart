0873ed527f16d9c3656b9adea2cee265375986f2
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-28 16:55:45 +0600

    Add DioNetworkService stub and exception handler mixin
    Introduces a stub implementation of DioNetworkService with unimplemented methods and a new ExceptionHandlerMixin file. These are initial scaffolds for remote data handling and exception management.

---
+++ lib/shared/data/remote/dio_network_service.dart
@@ -0,0 +1,36 @@
+//
+//
+//
+// import 'package:flutter/foundation.dart';
+// import 'package:riverpod_standard/shared/data/remote/network_service.dart';
+// import 'package:riverpod_standard/shared/domain/models/either.dart';
+// import 'package:riverpod_standard/shared/domain/models/response.dart';
+// import 'package:riverpod_standard/shared/exceptions/http_exception.dart';
+//
+// class DioNetworkService extends NetworkService with ExceptionHandlerMixin {
+//   @override
+//   // TODO: implement baseUrl
+//   String get baseUrl => throw UnimplementedError();
+//
+//   @override
+//   Future<Either<AppException, Response>> get(String endpoint, {Map<String, dynamic>? queryParameters}) {
+//     // TODO: implement get
+//     throw UnimplementedError();
+//   }
+//
+//   @override
+//   // TODO: implement headers
+//   Map<String, Object> get headers => throw UnimplementedError();
+//
+//   @override
+//   Future<Either<AppException, Response>> post(String endpoint, {Map<String, dynamic>? data}) {
+//     // TODO: implement post
+//     throw UnimplementedError();
+//   }
+//
+//   @override
+//   void updateHeader(Map<String, dynamic> data) {
+//     // TODO: implement updateHeader
+//   }
+//
+// }
