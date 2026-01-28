d94081441ed7624ff7c0f115242c2f384b5bcfdf
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-01-28 16:43:59 +0600

    Update network_service.dart

--- lib/shared/data/remote/network_service.dart
+++ lib/shared/data/remote/network_service.dart
@@ -1,8 +1,20 @@
-
-
+import '../../domain/models/either.dart';
+import '../../domain/models/response.dart';
+import '../../exceptions/http_exception.dart';
 
 abstract class NetworkService {
   String get baseUrl;
   Map<String, Object> get headers;
   void updateHeader(Map<String, dynamic> data);
+
+
+  Future<Either<AppException, Response>> get(
+    String endpoint, {
+    Map<String, dynamic>? queryParameters,
+  });
+
+  Future<Either<AppException, Response>> post(
+    String endpoint, {
+    Map<String, dynamic>? data,
+  });
 }
