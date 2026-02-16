69c6fe3d36cbd92cd9cf8a078c1411647a8fb9b7
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-02-16 14:47:51 +0600

    add dashboard_repository.dart

--- lib/features/dashboard/presentation/providers/state/dashboard_notifier.dart
+++ lib/features/dashboard/presentation/providers/state/dashboard_notifier.dart
@@ -1,9 +1,10 @@
-
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_standard/features/dashboard/presentation/providers/state/dashboard_state.dart';
+import '../../../domain/repositories/dashboard_repository.dart';
 
-class DashboardNotifier extends StateNotifier<DashboardState>{
-  DashboardNotifier(super.state);
+class DashboardNotifier extends StateNotifier<DashboardState> {
+  final DashboardRepository dashboardRepository;
 
+  DashboardNotifier(this.dashboardRepository)
+    : super(const DashboardState.initial());
 }
