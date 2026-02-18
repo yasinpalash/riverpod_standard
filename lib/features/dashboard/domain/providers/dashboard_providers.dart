5016727d1e1cfa3a6242081094f3de59015ef3fe
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-02-18 15:56:39 +0600

    updated

---
+++ lib/features/dashboard/domain/providers/dashboard_providers.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../shared/data/remote/network_service.dart';
+
+// final dashboardDatasourceProvider =
+// Provider.family<DashboardDatasource, NetworkService>(
+//       (_, networkService) => DashboardRemoteDatasource(networkService),
+// );
---
+++ lib/features/dashboard/presentation/providers/dashboard_state_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_standard/features/dashboard/presentation/providers/state/dashboard_notifier.dart';
+import 'package:riverpod_standard/features/dashboard/presentation/providers/state/dashboard_state.dart';
+
+// final dashboardNotifierProvider =
+// StateNotifierProvider<DashboardNotifier, DashboardState>((ref) {
+//   // final repository = ref.watch(dashboardRepositoryProvider);
+//   // return DashboardNotifier(repository)..fetchProducts();
+// });
