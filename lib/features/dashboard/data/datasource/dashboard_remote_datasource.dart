37fd1f0f84410f37ae5599c98508b2b6f9da32f2
Yasin Mia palash <yasinmiapolash@gmail.com>
2026-02-19 11:31:11 +0600

    dashboarddatasource

--- lib/features/dashboard/data/datasource/dashboard_remote_datasource.dart
+++ lib/features/dashboard/data/datasource/dashboard_remote_datasource.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_standard/shared/domain/models/paginated_response.dart';
 import 'package:riverpod_standard/shared/exceptions/http_exception.dart';
 
 import '../../../../shared/data/remote/network_service.dart';
+import '../../../../shared/globals.dart';
 
 abstract class DashboardDatasource {
   Future<Either<AppException, PaginatedResponse>> fetchPaginatedProducts({
@@ -19,16 +20,59 @@ class DashboardRemoteDatasource extends DashboardDatasource {
   DashboardRemoteDatasource(this.networkService);
 
   @override
-  Future<Either<AppException, PaginatedResponse>>
-  fetchPaginatedProducts({required int skip})  async{
-    // TODO: implement fetchPaginatedProduct
-    throw UnimplementedError();
+  Future<Either<AppException, PaginatedResponse>> fetchPaginatedProducts({
+    required int skip,
+  }) async {
+    final response = await networkService.get(
+      '/products',
+      queryParameters: {'skip': skip, 'limit': PRODUCTS_PER_PAGE},
+    );
+
+    return response.fold((l) => Left(l), (r) {
+      final jsonData = r.data;
+      if (jsonData == null) {
+        return Left(
+          AppException(
+            identifier: 'fetchPaginatedData',
+            statusCode: 0,
+            message: 'The data is not in the valid format.',
+          ),
+        );
+      }
+      final paginatedResponse = PaginatedResponse.fromJson(
+        jsonData,
+        jsonData['products'] ?? [],
+      );
+      return Right(paginatedResponse);
+    });
   }
 
   @override
-  Future<Either<AppException, PaginatedResponse<dynamic>>>
-  searchPaginatedProducts({required int skip, required String query}) {
-    // TODO: implement searchPaginatedProducts
-    throw UnimplementedError();
+  Future<Either<AppException, PaginatedResponse>> searchPaginatedProducts({
+    required int skip,
+    required String query,
+  }) async {
+    final response = await networkService.get(
+      '/products/search?q=$query',
+      queryParameters: {'skip': skip, 'limit': PRODUCTS_PER_PAGE},
+    );
+
+    return response.fold((l) => Left(l), (r) {
+      final jsonData = r.data;
+      if (jsonData == null) {
+        return Left(
+          AppException(
+            identifier: 'search PaginatedData',
+            statusCode: 0,
+            message: 'The data is not in the valid format.',
+          ),
+        );
+      }
+      final paginatedResponse = PaginatedResponse.fromJson(
+        jsonData,
+        jsonData['products'] ?? [],
+      );
+      return Right(paginatedResponse);
+    });
   }
 }
